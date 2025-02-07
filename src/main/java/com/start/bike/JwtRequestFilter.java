2524fbe6e28a35d2524ba66059602e351e2dc662
alice <3524398813@qq.com>
2025-02-07 16:41:12 +0800

    jwt

---
+++ src/main/java/com/start/bike/JwtRequestFilter.java
@@ -0,0 +1,37 @@
+package com.start.bike;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+
+        String username = null;
+        String jwt = null;
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            jwt = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(jwt);
+        }
+
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // 这里可以添加进一步的逻辑来验证用户名
+            // 如果令牌有效，则设置认证信息
+        }
+        chain.doFilter(request, response);
+    }
+}
--- src/main/java/com/start/bike/controller/UserController.java
+++ src/main/java/com/start/bike/controller/UserController.java
@@ -2,16 +2,21 @@ package com.start.bike.controller;
 import com.start.bike.service.UserService;
 import com.start.bike.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/User")
 public class UserController {
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    @GetMapping
+    @PostMapping("/login")
     public List<User> getUsers(){
         return userService.getUsers();
     }
